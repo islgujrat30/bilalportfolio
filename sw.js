8c834100476ae56a16dd8be420013d447e862de1
islgujrat30 <islgujrat30@gmail.com>
2026-04-23 07:52:59 +0000

    Fix paths in sw.js

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('portfolio-cache').then(function(cache) {
       return cache.addAll([
-        '/bilalportfolio/',
-        '/bilalportfolio/index.html',
-        '/bilalportfolio/styles.css',
-        '/bilalportfolio/script.js',
-        '/bilalportfolio/bilal.jpg',
-        '/bilalportfolio/manifest.json'
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/bilal.jpg',
+        '/manifest.json'
       ]);
     })
   );
